41fb992a8128fcced65caf6cf6c9f97ccdbbdd27
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-10-07 00:03:43 -0700

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001856507861))
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL',  ))
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001856507861))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', '')
 HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://youtu.be/2biEhc5b1pY')
 
